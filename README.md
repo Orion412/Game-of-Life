8f6a6eaf7a9a5e790bc03e84f010a86ae7cd388b
Orion412 <gart.alt1@gmail.com>
2021-08-18 14:24:42 -0500

    game works, fix edges

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ made with p5.js & JavaScript
 Also simply known as "Life", CGoL is a Turing complete cellular automaton made in 1970 by British mathematician John Horton Conway.
 
 --RULES--
-The universe is an infinite*, 2-dimensional, orthogonal grid of square "cells" with 2 potential states: "live" or "dead" (defined by a 1 or 0 in this example)
+
+The universe is an infinite, 2-dimensional, orthogonal grid of square "cells" with 2 potential states: "live" or "dead" (white or black)
 Every "cell" interacts with it's 8 neighbors (above, below, left, right, and 4 diagonal) with each step in time (aka every "generation").
 There is no input required, besides creating the initial state ("seed").
 The following rules are applied to every "cell", every "generation":
--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+//create array of colums and rows
 function createArray(cols, rows) {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
@@ -6,10 +7,11 @@ function createArray(cols, rows) {
   return arr;
 }
 
+//create canvas to display array as a grid
 let grid;
 let cols;
 let rows;
-let resolution = 10;
+let resolution = 10; //size of cells
 
 function setup() {
   createCanvas(1000, 600);
@@ -19,22 +21,61 @@ function setup() {
   grid = createArray(cols, rows);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      grid[i][j] = floor(random(2));
+      grid[i][j] = floor(random(2)); //random seeding of cells
     }
   }
 }
 
+//fill in living and dead cells
 function draw() {
-  background(0)
+  background(0); //dead cells
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       let x = i * resolution;
       let y = j * resolution;
       if (grid[i][j] == 1) {
-        fill(255);
-
-      rect(x, y, resolution, resolution)
+        fill(255); //live cells
+        rect(x, y, resolution, resolution);
       }
     }
   }
+
+  //compute next generation based on previous
+  let next = createArray(cols, rows);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      //edges keep same value as previous generation, will change later to wrap
+      if (i == 0 || i == cols - 1 || j == 0 || j == rows - 1) {
+        next[i][j] = state;
+      } else {
+        //count LIVE neighbors
+        let sum = 0;
+        let neighbors = countNeighbors(grid, i, j);
+        if (state == 0 && neighbors == 3) {
+          next[i][j] = 1;
+        } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+          next[i][j] = 0;
+        } else {
+          next[i][j] = state;
+        }
+      }
+    }
+  }
+  grid = next;
+}
+
+//count ALL neighbors
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      sum += grid[x + i][y + j];
+    }
+  }
+
+  //cell doesnt count self as neighbor
+  sum -= grid[x][y];
+  return sum;
 }
